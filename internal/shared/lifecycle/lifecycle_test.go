86e04470ec42a8c1918d354e4064774f9cef6dc0
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-09-16 12:07:10 +0200

    Refactor lifecycle unit test

--- internal/shared/lifecycle/lifecycle_test.go
+++ internal/shared/lifecycle/lifecycle_test.go
@@ -2,25 +2,27 @@ package lifecycle
 
 import (
 	"os"
+	"sync"
 	"syscall"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_GivenTerminationSignal_WhenListenForShutdown_ThenCallsTheFunctionPassed(t *testing.T) {
 	shutdownSignal := make(chan os.Signal, 1)
-
+	var wg sync.WaitGroup
 	shutdownFuncCalled := false
 	shutdownFunc := func() {
+		defer wg.Done()
 		shutdownFuncCalled = true
 	}
+	wg.Add(1)
 
 	go ListenForApplicationShutDown(shutdownFunc, shutdownSignal)
 
 	shutdownSignal <- syscall.SIGTERM
+	wg.Wait()
 
-	time.Sleep(time.Second)
 	assert.True(t, shutdownFuncCalled)
 }
